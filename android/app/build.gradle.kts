af6090f73426a5c6a8699699535da62fdbedd7fd
HugehardJiang <83503393+HugehardJiang@users.noreply.github.com>
2026-08-25 18:00:13 +0800

    Release v0.3.84 and Android v0.1.68

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -10,8 +10,8 @@ android {
         applicationId = "cn.neu.zhizhangdongda"
         minSdk = 24
         targetSdk = 35
-        versionCode = 67
-        versionName = "0.1.67"
+        versionCode = 68
+        versionName = "0.1.68"
     }
 
     buildTypes {
